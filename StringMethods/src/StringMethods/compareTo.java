52d6e5ad54bd0c0ff5b42571d6267abf00ae08c1
User <User@192.168.81.61>
2021-12-03 11:26:31 +0530

    done to check

--- StringMethods/src/StringMethods/compareTo.java
+++ StringMethods/src/StringMethods/compareTo.java
@@ -3,7 +3,7 @@ package StringMethods;
 public class compareTo {
 
 		  public static void main(String[] args) {
-		    String myStr1 = "Hello";
+		    String myStr1 = "HelloSaras";
 		    String myStr2 = "Hello";
 		    System.out.println(myStr1.compareTo(myStr2)); // Returns 0 because they are equal
 		  }
